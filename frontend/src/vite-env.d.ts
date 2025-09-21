7b3f47a91bd127cd9a210d6bf689cb210ad7895a
Isaiah Chia <130481550+Isaidchia@users.noreply.github.com>
2025-09-22 02:27:09 +0800

    fixed all build errors

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,18 +1,2 @@
 /// <reference types="vite/client" />
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      group: any
-      mesh: any
-      sphereGeometry: any
-      meshBasicMaterial: any
-      meshStandardMaterial: any
-      ringGeometry: any
-      ambientLight: any
-      directionalLight: any
-      pointLight: any
-      hemisphereLight: any
-    }
-  }
-}
+/// <reference path="./@types/react-three-fiber.d.ts" />
